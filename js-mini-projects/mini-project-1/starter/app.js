e0d3dbbbf38f249ea7c459c600a8a599aa4d88ac
Muhammad Hafiz Hisbullah <hafichever@gmail.com>
2024-01-21 11:50:46 +0700

    cara penulisan variabel di javascript

--- js-mini-projects/mini-project-1/starter/app.js
+++ js-mini-projects/mini-project-1/starter/app.js
@@ -1,3 +1,31 @@
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
+
+var thisIsFunction = "this is function"; // <== untuk membuat sebuah variabel bebas
+const Pi = 3.14; // <== const untuk variabel yang tidak dapat diubah
+
+// var x = y + x;
+
+function sumNum(a, b) {
+  return a + b;
+}
+
+var iniSebuahVar = "var";
+
+IniSebuahVar = "hadeh";
+
+console.log(IniSebuahVar);
+
+var arr = [
+	"Bubur", 
+	"Bakmi", 
+	"Ayam", 
+	34, 
+	"300", 
+	"Sapi", 
+	"Barbeque", 
+	1922
+];
+
+ 
