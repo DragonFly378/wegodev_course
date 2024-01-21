3f922b95a03f7f662a902ff6c6c12616a84a5b20
Muhammad Hafiz Hisbullah <hafichever@gmail.com>
2024-01-21 18:17:57 +0700

    re-assign variables

--- js-mini-projects/mini-project-1/starter/app.js
+++ js-mini-projects/mini-project-1/starter/app.js
@@ -1,24 +1,10 @@
+var numberA = 100;
+numberA = 120
+
+var numberB = numberA + 20
+numberB = "hadeh"
+
+console.log(numberB)
+
 const projectTitle = document.querySelector("#project-title");
-
 projectTitle.textContent = "Title Baru dari JS File";
-
-var thisIsFunction = "this is function"; // <== untuk membuat sebuah variabel bebas
-const Pi = 3.14; // <== const untuk variabel yang tidak dapat diubah
-
-// var x = y + x;
-
-function sumNum(a, b) {
-  return a + b;
-}
-
-var y = 20
-var x = 2
-var x = y + x;
-console.log(x)
-
-var iniSebuahVar = "var";
-IniSebuahVar = "hadeh";
-
-console.log(iniSebuahVar);
-
- 
